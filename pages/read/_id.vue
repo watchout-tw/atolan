394d4da7aed3796e822bd36827e4f4ae68d00172
chihaoyo <chihaoyo@gmail.com>
2019-02-18 15:30:32 +0800

    Add /read/_id & other placeholder index pages

---
+++ pages/read/_id.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="page read single">
+  <doc-header :doc="doc" :cachedAuthors="cachedAuthors" />
+  <ghost-article :article="doc.content" :footnotes="footnotes" :links="links" :references="references" :data="dataOnReferences" />
+  <div class="share margin-top-bottom-double">
+    <div class="section-title with-underline text-align-center margin-top-bottom-single"><span>認同請分享</span></div>
+    <share-to-platforms :url="shareURL" />
+  </div>
+</div>
+</template>
+
+<script>
+import * as info from '~/data/info'
+import * as firestore from 'watchout-common-functions/lib/firestore'
+import { knowsCaching, knowsWatchout } from 'watchout-common-functions/interfaces'
+import { mobiledocProcessor } from 'watchout-common-functions/lib/bunko'
+import DocHeader from 'watchout-common-functions/components/comp/DocHeader'
+import GhostArticle from 'watchout-common-functions/components/ghost/Article'
+import ShareToPlatforms from 'watchout-common-functions/components/ShareToPlatforms'
+import defaultCoverImage from 'watchout-common-assets/images/default-cover-images/uc-2-1.jpg'
+
+export default {
+  mixins: [knowsCaching, knowsWatchout],
+  async asyncData({ params, error }) {
+    let doc = await firestore.bunko.getDoc(params.id, true)
+    if(!(doc && doc.publishedTo === info.CHANNEL_ID)) { // FIXME: better error handling
+      error({ statusCode: 404 })
+      return
+    }
+    let mobiledoc = JSON.parse(doc.content.mobiledoc)
+    let processed = await mobiledocProcessor(mobiledoc)
+    return Object.assign({
+      doc,
+      mobiledoc
+    }, processed)
+  },
+  head() {
+    const pageTitle = this.doc.title + info.SEPARATOR + info.SITE_TITLE
+    const pageDescription = this.doc.description
+    const pageImage = this.doc.imageObj ? this.doc.imageObj.permalink : defaultCoverImage
+    return {
+      title: pageTitle,
+      meta: this.generateMeta('uc', pageTitle, pageDescription, pageImage)
+    }
+  },
+  computed: {
+    shareURL() {
+      return this.getCompDocURL('uc', this.$route.params.id)
+    }
+  },
+  components: {
+    DocHeader,
+    GhostArticle,
+    ShareToPlatforms
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~watchout-common-assets/styles/resources';
+
+.page.read.single {
+}
+</style>
--- pages/read/index.vue
+++ pages/read/index.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="page index">
-  <div class="tcl-container">
-    <div class="tcl-panel full-width with-padding">
-      <no-ssr><welcome /></no-ssr>
+<div class="page project read">
+  <div class="tcl-container margin-top-bottom-8">
+    <div class="tcl-panel tcl-left-right-margin with-top-bottom-margin">
+      A reader lives a thousand lives before he dies. The man who never reads lives only one.
     </div>
   </div>
 </div>
@@ -11,21 +11,17 @@
 <script>
 import * as info from '~/data/info'
 import { knowsWatchout } from 'watchout-common-functions/interfaces'
-import Welcome from 'watchout-common-functions/components/Welcome'
 import defaultCoverImage from 'watchout-common-assets/images/default-cover-images/uc-2-1.jpg'
 
 export default {
   mixins: [knowsWatchout],
   head() {
-    let pageTitle = info.SITE_TITLE
-    let pageDescription = info.SITE_DESCRIPTION
+    const pageTitle = '閱讀' + info.SEPARATOR + info.SITE_TITLE
+    const pageDescription = info.SITE_DESCRIPTION
     return {
       title: pageTitle,
       meta: this.generateMeta('uc', pageTitle, pageDescription, defaultCoverImage)
     }
-  },
-  components: {
-    Welcome
   }
 }
 </script>
