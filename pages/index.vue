850563302f4ce60721f8c18a3c6f8c6d8106428a
chihaoyo <chihaoyo@gmail.com>
2019-02-27 16:30:05 +0800

    Add doc list to index page

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,45 @@
       <no-ssr><welcome /></no-ssr>
     </div>
   </div>
+  <div class="docs first-doc margin-top-bottom-8">
+    <reference-preview :reference="docRefs[0]" :data="dataOnReferences" display="tcl" :show-pub-dest="true" />
+  </div>
+  <div class="docs tcl-container margin-top-bottom-4">
+    <div class="doc tcl-panel tcl-left-right-margin with-top-bottom-margin" :class="{ 'half-width': index >= 4 }" v-for="(docRef, index) of docRefs" :key="index" v-if="index > 0">
+      <reference-preview :reference="docRef" :data="dataOnReferences" :display="index >= 4 ? 'vertical' : null" :show-pub-dest="true" />
+    </div>
+    <div class="tcl-panel half-width"></div>
+    <div class="tcl-panel half-width"></div>
+    <div class="tcl-panel half-width"></div>
+  </div>
 </div>
 </template>
 
 <script>
+import * as firestore from 'watchout-common-functions/lib/firestore'
 import * as info from '~/data/info'
 import { knowsWatchout } from 'watchout-common-functions/interfaces'
+import { makeReference } from 'watchout-common-functions/lib/watchout'
 import Welcome from 'watchout-common-functions/components/Welcome'
+import ReferencePreview from 'watchout-common-functions/components/ReferencePreview'
 import defaultCoverImage from 'watchout-common-assets/images/default-cover-images/uc-2-1.jpg'
 
 export default {
   mixins: [knowsWatchout],
+  async asyncData() {
+    // get docs with pub-dest uc
+    let docs = await firestore.bunko.getDocs({ pubDest: 'uc' })
+    let docRefs = docs.map(doc => makeReference('doc', doc.id, { publishedTo: doc.publishedTo }))
+    let dataOnReferences = {}
+    for(let i = 0; i < docRefs.length; i++) {
+      dataOnReferences[docRefs[i].url] = docs[i]
+    }
+
+    return {
+      docRefs,
+      dataOnReferences
+    }
+  },
   head() {
     let pageTitle = info.SITE_TITLE
     let pageDescription = info.SITE_DESCRIPTION
@@ -24,8 +52,14 @@ export default {
       meta: this.generateMeta('uc', pageTitle, pageDescription, defaultCoverImage)
     }
   },
+  methods: {
+    getModule(id) {
+      return this.modules.find(module => module.id === id)
+    }
+  },
   components: {
-    Welcome
+    Welcome,
+    ReferencePreview
   }
 }
 </script>
